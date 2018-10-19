55bfdfb7b4e4708fa8c4fd17c167af5d15e26931
Elliot Courant <elliot.courant@readystock.io>
2018-10-18 20:49:05 -0500

    Added transaction statement deparsing, removed unused old deparsing code

--- deparse.go
+++ deparse.go
@@ -1,24 +1,9 @@
 package pg_query
 
 import (
-	"fmt"
 	pq "github.com/Ready-Stock/pg_query_go/nodes"
 	"github.com/kataras/go-errors"
 	"reflect"
-	"strings"
-)
-
-type contextType int64
-
-const (
-	True      contextType = 1
-	False     contextType = 2
-	Select    contextType = 4
-	Update    contextType = 8
-	A_CONST   contextType = 16
-	FUNC_CALL contextType = 32
-	TYPE_NAME contextType = 64
-	Operator  contextType = 128
 )
 
 func DeparseValue(aconst pq.A_Const) (interface{}, error) {
@@ -32,62 +17,4 @@ func DeparseValue(aconst pq.A_Const) (interface{}, error) {
 	default:
 		return nil, errors.New("cannot parse type %s").Format(reflect.TypeOf(c).Name())
 	}
-}
-
-func deparse_item(n pq.Node, ctx *contextType) (*string, error) {
-	switch node := n.(type) {
-	case pq.TransactionStmt:
-		return deparse_transaction(node)
-	default:
-		return nil, errors.New("cannot deparse node type %s").Format(reflect.TypeOf(node).String())
-	}
-}
-
-func deparse_item_list(nodes []pq.Node, ctx *contextType) ([]string, error) {
-	out := make([]string, len(nodes))
-	for i, node := range nodes {
-		if str, err := deparse_item(node, ctx); err != nil {
-			return nil, err
-		} else {
-			out[i] = *str
-		}
-	}
-	return out, nil
-}
-
-var transactionCmds = map[pq.TransactionStmtKind]string{
-	pq.TRANS_STMT_BEGIN:             "BEGIN",
-	pq.TRANS_STMT_START:             "BEGIN",
-	pq.TRANS_STMT_COMMIT:            "COMMIT",
-	pq.TRANS_STMT_ROLLBACK:          "ROLLBACK",
-	pq.TRANS_STMT_SAVEPOINT:         "SAVEPOINT",
-	pq.TRANS_STMT_RELEASE:           "RELEASE",
-	pq.TRANS_STMT_ROLLBACK_TO:       "ROLLBACK TO SAVEPOINT",
-	pq.TRANS_STMT_PREPARE:           "PREPARE TRANSACTION",
-	pq.TRANS_STMT_COMMIT_PREPARED:   "COMMIT TRANSACTION",
-	pq.TRANS_STMT_ROLLBACK_PREPARED: "ROLLBACK TRANSACTION",
-}
-
-func deparse_transaction(node pq.TransactionStmt) (*string, error) {
-	out := make([]string, 0)
-	if kind, ok := transactionCmds[node.Kind]; !ok {
-		return nil, errors.New("couldn't deparse transaction kind: %s").Format(node.Kind)
-	} else {
-		out = append(out, kind)
-	}
-
-	if node.Kind == pq.TRANS_STMT_PREPARE ||
-		node.Kind == pq.TRANS_STMT_COMMIT_PREPARED ||
-		node.Kind == pq.TRANS_STMT_ROLLBACK_PREPARED {
-		if node.Gid != nil {
-			out = append(out, fmt.Sprintf("'%s'", *node.Gid))
-		}
-	} else {
-		if node.Options.Items != nil && len(node.Options.Items) > 0 {
-
-		}
-	}
-
-	result := strings.Join(out, " ")
-	return &result, nil
 }
--- nodes/transaction_stmt_deparse.go
+++ nodes/transaction_stmt_deparse.go
@@ -2,6 +2,32 @@
 
 package pg_query
 
+import (
+	"fmt"
+	"github.com/juju/errors"
+	"strings"
+)
+
 func (node TransactionStmt) Deparse(ctx Context) (*string, error) {
-	panic("Not Implemented")
+	out := make([]string, 0)
+	if kind, ok := transactionCmds[node.Kind]; !ok {
+		return nil, errors.Errorf("couldn't deparse transaction kind: %s", node.Kind)
+	} else {
+		out = append(out, kind)
+	}
+
+	if node.Kind == TRANS_STMT_PREPARE ||
+		node.Kind == TRANS_STMT_COMMIT_PREPARED ||
+		node.Kind == TRANS_STMT_ROLLBACK_PREPARED {
+		if node.Gid != nil {
+			out = append(out, fmt.Sprintf("'%s'", *node.Gid))
+		}
+	} else {
+		if node.Options.Items != nil && len(node.Options.Items) > 0 {
+
+		}
+	}
+
+	result := strings.Join(out, " ")
+	return &result, nil
 }
