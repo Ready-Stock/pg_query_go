06a3954216c2874a42204493cd1f7cc667d7592d
Elliot Courant <elliot.courant@readystock.io>
2018-10-18 20:25:04 -0500

    Added select statement deparsing.

--- nodes/select_stmt_deparse.go
+++ nodes/select_stmt_deparse.go
@@ -2,6 +2,147 @@
 
 package pg_query
 
+import (
+	"strings"
+)
+
 func (node SelectStmt) Deparse(ctx Context) (*string, error) {
-	panic("Not Implemented")
+	out := make([]string, 0)
+	if node.Op == SETOP_UNION {
+		if str, err := deparseNode(node.Larg, Context_None); err != nil {
+			return nil, err
+		} else {
+			out = append(out, *str)
+		}
+
+		out = append(out, "UNION")
+		if node.All {
+			out = append(out, "ALL")
+		}
+
+		if str, err := deparseNode(node.Rarg, Context_None); err != nil {
+			return nil, err
+		} else {
+			out = append(out, *str)
+		}
+
+		result := strings.Join(out, " ")
+		return &result, nil
+	}
+
+	if node.WithClause != nil {
+		if str, err := deparseNode(node.WithClause, Context_None); err != nil {
+			return nil, err
+		} else {
+			out = append(out, *str)
+		}
+	}
+
+	// Get select *distinct* *fields*
+	if node.TargetList.Items != nil && len(node.TargetList.Items) > 0 {
+		out = append(out, "SELECT")
+		if node.DistinctClause.Items != nil && len(node.DistinctClause.Items) > 0 {
+			out = append(out, "DISTINCT")
+		}
+		fields := make([]string, len(node.TargetList.Items))
+		for i, field := range node.TargetList.Items {
+			if str, err := deparseNode(field, Context_Select); err != nil {
+				return nil, err
+			} else {
+				fields[i] = *str
+			}
+		}
+		out = append(out, strings.Join(fields, ", "))
+	}
+
+	if node.FromClause.Items != nil && len(node.FromClause.Items) > 0 {
+		out = append(out, "FROM")
+		froms := make([]string, len(node.FromClause.Items))
+		for i, from := range node.FromClause.Items {
+			if str, err := deparseNode(from, Context_Select); err != nil {
+				return nil, err
+			} else {
+				froms[i] = *str
+			}
+		}
+		out = append(out, strings.Join(froms, ", "))
+	}
+
+	if node.WhereClause != nil {
+		out = append(out, "WHERE")
+		if str, err := deparseNode(node.WhereClause, Context_None); err != nil {
+			return nil, err
+		} else {
+			out = append(out, *str)
+		}
+	}
+
+	if node.ValuesLists != nil && len(node.ValuesLists) > 0 {
+		out = append(out, "VALUES")
+		for _, valuelist := range node.ValuesLists {
+			values := make([]string, len(valuelist))
+			for i, value := range valuelist {
+				if str, err := deparseNode(value, Context_None); err != nil {
+					return nil, err
+				} else {
+					values[i] = *str
+				}
+			}
+			out = append(out, "("+strings.Join(values, ", ")+")")
+		}
+	}
+
+	if node.GroupClause.Items != nil && len(node.GroupClause.Items) > 0 {
+		out = append(out, "GROUP BY")
+		groups := make([]string, len(node.GroupClause.Items))
+		for i, group := range node.GroupClause.Items {
+			if str, err := deparseNode(group, Context_None); err != nil {
+				return nil, err
+			} else {
+				groups[i] = *str
+			}
+		}
+		out = append(out, strings.Join(groups, ", "))
+	}
+
+	if node.HavingClause != nil {
+		if str, err := deparseNode(node.HavingClause, Context_None); err != nil {
+			return nil, err
+		} else {
+			out = append(out, *str)
+		}
+	}
+
+	// Sort clause
+
+	if node.LimitCount != nil {
+		out = append(out, "LIMIT")
+		if str, err := deparseNode(node.LimitCount, Context_None); err != nil {
+			return nil, err
+		} else {
+			out = append(out, *str)
+		}
+	}
+
+	if node.LimitOffset != nil {
+		out = append(out, "OFFSET")
+		if str, err := deparseNode(node.LimitOffset, Context_None); err != nil {
+			return nil, err
+		} else {
+			out = append(out, *str)
+		}
+	}
+
+	if node.LockingClause.Items != nil && len(node.LockingClause.Items) > 0 {
+		for _, lock := range node.LockingClause.Items {
+			if str, err := deparseNode(lock, Context_None); err != nil {
+				return nil, err
+			} else {
+				out = append(out, *str)
+			}
+		}
+	}
+
+	result := strings.Join(out, " ")
+	return &result, nil
 }
