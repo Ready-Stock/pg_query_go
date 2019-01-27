97af7d899e2dc855b0e16bdc683c2bf1c8637e18
Elliot Courant <elliot.courant@readystock.io>
2019-01-27 11:03:24 -0600

    Added utility function to func_call to retrieve the name easily.

--- nodes/func_call_deparse.go
+++ nodes/func_call_deparse.go
@@ -20,37 +20,9 @@ func (node FuncCall) Deparse(ctx Context) (*string, error) {
 		args = append(args, "*")
 	}
 
-	difference := func(slice1 []string, slice2 []string) []string {
-		var diff []string
-		// Loop two times, first to find slice1 strings not in slice2,
-		// second loop to find slice2 strings not in slice1
-		for i := 0; i < 2; i++ {
-			for _, s1 := range slice1 {
-				found := false
-				for _, s2 := range slice2 {
-					if s1 == s2 {
-						found = true
-						break
-					}
-				}
-				// String not found. We add it to return slice
-				if !found {
-					diff = append(diff, s1)
-				}
-			}
-			// Swap the slices, only if it was the first loop
-			if i == 0 {
-				slice1, slice2 = slice2, slice1
-			}
-		}
-		return diff
-	}
-
-	name := ""
-	if names, err := deparseNodeList(node.Funcname.Items, Context_FuncCall); err != nil {
+	funcName, err := node.Name()
+	if err != nil {
 		return nil, err
-	} else {
-		name = strings.Join(difference([]string{"pg_catalog"}, names), ".")
 	}
 
 	distinct := ""
@@ -58,7 +30,7 @@ func (node FuncCall) Deparse(ctx Context) (*string, error) {
 		distinct = "DISTINCT "
 	}
 
-	out = append(out, fmt.Sprintf("%s(%s%s)", name, distinct, strings.Join(args, ", ")))
+	out = append(out, fmt.Sprintf("%s(%s%s)", funcName, distinct, strings.Join(args, ", ")))
 
 	if node.Over != nil {
 		if over, err := deparseNode(node.Over, Context_None); err != nil {
